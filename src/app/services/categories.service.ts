9a18df6417ced37ce37d03597428f9dd1f07eec3
havababay <hava.babay@gmail.com>
2024-01-20 20:16:26 +0200

    phase 3 - use local storage, need to fix date column

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,3 +1,4 @@
+import { categories } from './../../shared/data/categories';
 import { Injectable } from '@angular/core';
 import { Category } from '../../shared/model/category';
 
@@ -5,34 +6,64 @@ import { Category } from '../../shared/model/category';
   providedIn: 'root'
 })
 export class CategoriesService {
-  private categories : Map<number, Category>;
-  private nextId = 1;
+  private readonly CATEGORIES_KEY = 'categories';
+  private readonly NEXT_ID_KEY = 'nextId';
 
-  constructor() { 
-    this.categories = new Map();
+  private getCategories() : Map<number, Category>{
+    let categoriesString = localStorage.getItem(this.CATEGORIES_KEY);
+
+    if (!categoriesString) {
+      return new Map<number, Category>();
+    } else {
+      return new Map<number, Category>(JSON.parse(categoriesString));
+    }
+  }
+
+  private getNextId() : number {
+    let nextIdString = localStorage.getItem(this.NEXT_ID_KEY); 
+
+    return nextIdString ? parseInt(nextIdString) : 0;
+  }
+
+  private setCategories(list : Map<number, Category>) : void {
+    localStorage.setItem(this.CATEGORIES_KEY, JSON.stringify(Array.from(list)));
+  }
+
+  private setNextId(id : number) : void {
+    localStorage.setItem(this.NEXT_ID_KEY, id.toString());
   }
 
   list() : Category[] {
-    return Array.from(this.categories.values());
+    return Array.from(this.getCategories().values());
   }
 
   get(id : number) : Category | undefined {
-    return this.categories.get(id);
+    return this.getCategories().get(id);
   }
 
   delete(id : number) : void {
-    this.categories.delete(id);
+    let categoriesMap = this.getCategories();
+    categoriesMap.delete(id);
+    this.setCategories(categoriesMap);
   }
 
   update(category : Category) : void {
+    let categoriesMap = this.getCategories();
+
     category.lastUpdateDate = new Date();
-    this.categories.set(category.id, category);
+    categoriesMap.set(category.id, category);
+
+    this.setCategories(categoriesMap);
   }
 
   add(category : Category) : void {
-    category.id = this.nextId;
+    category.id = this.getNextId();
     category.lastUpdateDate = new Date();
-    this.categories.set(category.id, category);
-    ++this.nextId;
+
+    let categoriesMap = this.getCategories();
+    categoriesMap.set(category.id, category);
+
+    this.setCategories(categoriesMap);
+    this.setNextId(++category.id);
   }
 }
